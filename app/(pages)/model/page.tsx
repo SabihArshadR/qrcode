c3b42f1315268b838c8694a1778fb54e94ae55b9
SabihArshadR <sabih042@gmail.com>
2026-02-12 21:46:52 +0500

    change

--- app/(pages)/model/page.tsx
+++ app/(pages)/model/page.tsx
@@ -1,11 +1,26 @@
 "use client"
-
+import { useRef } from "react"
+import { useFrame } from "@react-three/fiber"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls, useGLTF } from "@react-three/drei"
+import * as THREE from "three"
 
 function Model() {
   const { scene } = useGLTF("/models/model.glb")
-  return <primitive object={scene} scale={1.5} />
+  const groupRef = useRef<THREE.Group>(undefined)
+
+  // Example: rotate continuously
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.rotation.y += 0.007
+    }
+  })
+
+  return (
+    <group ref={groupRef} scale={1.5} position={[0, 0, 0]}>
+      <primitive object={scene} />
+    </group>
+  )
 }
 
 export default function ModelViewer() {
--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,12 +1,13 @@
-"use client"
+"use client";
 
-import { QRCodeSVG } from "qrcode.react"
+import { QRCodeSVG } from "qrcode.react";
 
 export default function QRPage() {
   return (
     <div style={{ padding: 40 }}>
       <h2>Scan to View 3D Model</h2>
+      {/* <QRCodeSVG value="http://localhost:3000/model" size={256} /> */}
       <QRCodeSVG value="https://qrcode-xi-liard.vercel.app/model" size={256} />
     </div>
-  )
+  );
 }
