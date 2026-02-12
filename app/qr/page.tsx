b51c64ebd9909604c7c56191ecf6a97b0b122f48
SabihArshadR <sabih042@gmail.com>
2026-02-12 17:35:24 +0500

    change

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -6,7 +6,7 @@ export default function QRPage() {
   return (
     <div style={{ padding: 40 }}>
       <h2>Scan to View 3D Model</h2>
-      <QRCodeSVG value="http://localhost:3000/model" size={256} />
+      <QRCodeSVG value="https://qrcode-xi-liard.vercel.app/model" size={256} />
     </div>
   )
 }
